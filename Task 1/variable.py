5a58b08430eccafe4bc9589dd24d4d070bc23e22
Nezuko05 <riyaarora0517@gmail.com>
2024-07-02 14:51:56 +0530

    variable task

--- Task 1/variable.py
+++ Task 1/variable.py
@@ -1,2 +1,22 @@
+# 1. Create a variable named pi and store the value 22/7 in it. Now check the data type of this variable.
 pi=22/7
-print(type(pi))
+print(type(pi)) 
+# It's returning a float datatype.
+
+#--------------------------------------------------------------------------------------------------#
+
+# 2. Create a variable called for and assign it a value 4. See what happens and find out the reason behind the behavior that you see.
+ for = 4
+ # It will throw an error as the "for" is a reserved keyword used for creating loops. As such, it cannot be used as a variable name.
+
+ #---------------------------------------------------------------------------------------------------#
+
+ # 3. Store the principal amount, rate of interest, and time in different variables and then calculate the Simple Interest for 3 years.
+#  Formula: Simple Interest = P x R x T / 100
+p=30
+r=20
+t=3 
+SI=p*r*t/100
+print(SI)
+
+#---------------------------------------------------------------------------------------------------#
